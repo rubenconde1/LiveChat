bf520bb9ede4ef881922d04d8f1e8a3ac1cc650a
Ruben <rubencnd96@gmail.com>
2023-02-01 16:33:38 +0100

    Metodos, Formulario y Repositorio Messages

--- LiveChat/src/Controller/MessagesController.php
+++ LiveChat/src/Controller/MessagesController.php
@@ -9,12 +9,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class MessagesController extends AbstractController
 {
     #[Route('messages/from/{toUserId}', name: 'app_messages')]
-    public function index(): Response
-    {
-        return $this->render('messages/index.html.twig', [
-            'controller_name' => 'MessagesController',
-        ]);
-    }
+    public function sentMessages(ManagerRegistry $doctrine, $fromUserid, $toUserid): JsonResponse
+{
+    $messageRepo = $doctrine->getRepository(Message::class);
+    $userRepo = $doctrine->getRepository(User::class);
+    $fromUser = $userRepo->find($fromUserid);
+    $toUser = $userRepo->find($toUserid);
+    $messages = $messageRepo->findConversation($fromUser, $toUser);
+
+    $data = array_map(function ($message) {
+        return [
+            'id' => $message->getFromUser()->getId(),
+            'text' => $message->getText(),
+            'timestamp' => $message->getTimestamp()->format('Y-m-d H:i:s'),
+        ];
+    }, $messages);
+
+    return new JsonResponse($data, Response::HTTP_OK);
+}
+
 
     #[Route('post/toUser/{toUserId}')]
     public function sendMessage()
