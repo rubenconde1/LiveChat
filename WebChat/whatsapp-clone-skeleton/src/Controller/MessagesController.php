3eed9e02e11e30909faeb40e6c8f60c7e99d4852
rubenconde1 <rubencnd96@gmail.com>
2023-02-15 08:13:06 +0100

    importadas librerías

--- WebChat/whatsapp-clone-skeleton/src/Controller/MessagesController.php
+++ WebChat/whatsapp-clone-skeleton/src/Controller/MessagesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
